56cbd42cd02f386fe1422b9abedc664c9a9e0a1f
Gerard Mortel <gerard.mortel@yahoo.com>
2019-09-09 00:38:03 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string firstName = "Gerard";
+    string lastName = "Mortel";
+    cout << "Hello " << firstName << " " << lastName << "!" << endl;
+    firstName = "Mylove";
+    cout << "Hello " << firstName << " " << lastName << "!" << endl;
+    return 0;
+}
